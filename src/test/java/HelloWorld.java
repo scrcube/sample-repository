3cbc4cd27d23c0608efff70f63c0f5ec2c25299f
Salvador Camacho <scrcube@gmail.com>
2016-02-08 21:34:16 -0800

    Initial Setup

---
+++ src/test/java/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Salvador Camacho on 2/8/2016.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello World!" to the terminal window.
+        System.out.println("Hello World!");
+    }
+}
